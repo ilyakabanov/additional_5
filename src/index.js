a055e3709b55ebfdc6bba768050d0f48e43ae7ef
ilya <ilya@ilyas-MBP.Dlink>
2018-09-22 17:36:07 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let condition = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+
+    let item = bracketsConfig[i].join('');
+    item = item.replace(/(\.|\^|\$|\*|\+|\?|\(|\)|\[|\]\{|\}|\\|\|)/g, '\\$&');
+    condition.push( item );
+  }
+
+  let re = new RegExp('('+ condition.join('|') + ')', 'i');
+
+  while (str.search(re) != -1 ) {
+    str = str.replace(re, '');
+  }
+
+  return !str;
 }
